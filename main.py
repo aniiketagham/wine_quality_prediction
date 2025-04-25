55e34ee2dd49de3fa0622d4c0c75de3eb9bffc05
AniketAgham <aniketagham1509@gmail.com>
2025-04-25 19:22:15 +0530

    data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.wine_quality import logger
 from src.wine_quality.pipelines.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.wine_quality.pipelines.data_validation_pipeline import DataValidationTrainingPipeline
+from src.wine_quality.pipelines.data_transformation_pipeline import DataTransformationTrainingPipeline
 
 STAGE_NAME= "Data Ingestion Stage"
 try:
@@ -17,6 +18,16 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME= "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.initiate_data_transformation()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
--- src/wine_quality/config/configuration.py
+++ src/wine_quality/config/configuration.py
@@ -1,7 +1,7 @@
 from src.wine_quality.constants import *
 from src.wine_quality.utils.common import read_yaml, create_directories
 
-from src.wine_quality.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from src.wine_quality.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
 
 class ConfigurationManager:
     def __init__(self,
@@ -41,3 +41,14 @@ class ConfigurationManager:
             
         )
         return data_validation_config
+
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config=self.config.data_transformation
+        create_directories([config.root_dir])
+        data_transformation_config=DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path
+        )
+        
+        return data_transformation_config
+    
