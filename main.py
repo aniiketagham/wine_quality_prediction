99e64bb058f720e26e4d3b5b1d3bdadc1a751589
AniketAgham <aniketagham1509@gmail.com>
2025-04-25 17:42:10 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from src.wine_quality import logger
+from src.wine_quality.pipelines.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.wine_quality.pipelines.data_validation_pipeline import DataValidationTrainingPipeline
 
-logger.info("Welcome to our custom logging wine_quality")
+STAGE_NAME= "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME= "Data Validation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/wine_quality/pipelines/data_validation_pipeline.py
@@ -0,0 +1,26 @@
+from src.wine_quality.config.configuration import ConfigurationManager
+from src.wine_quality.components.data_validation import DataValidation
+from src.wine_quality import logger
+
+STAGE_NAME= "Data Validation Stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def initiate_data_ingestion(self):
+        config=ConfigurationManager()
+        data_validation_config=config.get_data_validation_config()
+        data_validation=DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+       
+            
+if __name__== '__main__'  :
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.initiate_data_ingestion()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
